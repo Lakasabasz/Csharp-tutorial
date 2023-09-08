2beb9e4032cf3f176414aa6631d4cb8147eba2ea
PlynDoChlodni <spamik@jba.pl>
2023-09-08 21:05:07 +0200

    Added list of randomly selected density values from the table

--- CloneTest/Program.cs
+++ CloneTest/Program.cs
@@ -2,28 +2,58 @@
 {
     private static void Main(string[] args)
     {
+        int nPeople; 
         int xDim, yDim;
         double x, y;
+        Random rnd = new Random();
 
         xDim = 10;  // (Dawniej a) Wymiar tablicy w osi x
         yDim = 10;  // (Dawniej b) Wymiar tablicy w osi y
 
-        double[,] desity = new double[xDim,yDim];
+        nPeople = 10;  //Liczba wylosowanych komórek
 
-        for (x = 0; x < xDim; x++) {
+
+
+        double[,] density = new double[xDim,yDim];  //Utworzenie tablicy gęstości zaludnienia
+
+
+        for (x = 0; x < xDim; x++) {  //Wypełnienie tablicy Gęstości zaludnienia danymi ze wzoru
             for (y = 0; y < yDim; y++)
             {
-                desity[(int)x,(int)y] = -(((x - 5) * (x - 5)) + ((y - 5) * (y - 5))) * ((((x - 1) * (x - 1)) + ((y - 3) * (y - 3))) / 8) + 4;
+                density[(int)x,(int)y] = -(((x - 5) * (x - 5)) + ((y - 5) * (y - 5))) * ((((x - 1) * (x - 1)) + ((y - 3) * (y - 3))) / 8) + 4;
             }
         }
 
+
+
+        var hubsDensity = new List<Tuple<int, int, double>>();  // inicjacja listy zapisujących dane o losowanych hubach początkowych
+
+        for (int i = 0; i < nPeople; i++) {  //Wybór losowych komórek z tablicy gęstości i zapisanie do listy hubsDensity
+            int xRnd = rnd.Next(0, xDim);
+            int yRnd = rnd.Next(0, yDim);
+            hubsDensity.Add(Tuple.Create(xRnd, yRnd, density[xRnd, yRnd]));
+        }
+
+        
+
+
+
+
+
+        /*
+        foreach (Tuple<int, int, double> tuple in hubsDensity) {
+            Console.WriteLine(tuple.Item1 + " " + tuple.Item2 + " " + tuple.Item3);
+        }
+        */
+        /*
         for (int i = 0; i < yDim; i++) {
             for (int j = 0; j < xDim; j++) {
-                Console.Write(desity[j,i]);
+                Console.Write(density[j,i]);
                 Console.Write(" ");
             }
             Console.WriteLine("");
         }
+        */
 
     }
 }
