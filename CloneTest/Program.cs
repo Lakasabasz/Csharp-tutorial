e318e2d35174b4b7041dc58f0874715a7a91732f
PlynDoChlodni <spamik@jba.pl>
2023-09-08 17:14:27 +0200

    Dodano "!" do linii 4

--- CloneTest/Program.cs
+++ CloneTest/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string? text = null;
-text ??= "Jeśli widzisz ten tekst to znaczy, że wszystko działa poprawnie";
+text ??= "Jeśli widzisz ten tekst to znaczy, że wszystko działa poprawnie!";
 Console.WriteLine(text);
