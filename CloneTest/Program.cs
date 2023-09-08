68c6f40edbef53edecc9a01759188a5b59e66820
PlynDoChlodni <spamik@jba.pl>
2023-09-08 19:18:39 +0200

    Creation of 1 of the 2D matrixes

--- CloneTest/Program.cs
+++ CloneTest/Program.cs
@@ -1,5 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int xDim, yDim;
+        double x, y;
 
-string? text = null;
-text ??= "Jeśli widzisz ten tekst to znaczy, że wszystko działa poprawnie!";
-Console.WriteLine(text);
+        xDim = 10;  // (Dawniej a) Wymiar tablicy w osi x
+        yDim = 10;  // (Dawniej b) Wymiar tablicy w osi y
+
+        double[,] desity = new double[xDim,yDim];
+
+        for (x = 0; x < xDim; x++) {
+            for (y = 0; y < yDim; y++)
+            {
+                desity[(int)x,(int)y] = -(((x - 5) * (x - 5)) + ((y - 5) * (y - 5))) * ((((x - 1) * (x - 1)) + ((y - 3) * (y - 3))) / 8) + 4;
+            }
+        }
+
+        for (int i = 0; i < yDim; i++) {
+            for (int j = 0; j < xDim; j++) {
+                Console.Write(desity[j,i]);
+                Console.Write(" ");
+            }
+            Console.WriteLine("");
+        }
+
+    }
+}
