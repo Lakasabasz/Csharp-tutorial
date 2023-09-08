30da40bd8fc15cc16fe8e4a4b6a25dbc6aa3362b
PlynDoChlodni <spamik@jba.pl>
2023-09-09 00:18:50 +0200

    Added calculation of CDF for elemnts from hubsDensity

--- CloneTest/Program.cs
+++ CloneTest/Program.cs
@@ -20,7 +20,7 @@
         for (x = 0; x < xDim; x++) {  //Wypełnienie tablicy Gęstości zaludnienia danymi ze wzoru
             for (y = 0; y < yDim; y++)
             {
-                density[(int)x,(int)y] = -(((x - 5) * (x - 5)) + ((y - 5) * (y - 5))) * ((((x - 1) * (x - 1)) + ((y - 3) * (y - 3))) / 8) + 4;
+                density[(int)x,(int)y] = -(((x - 5) * (x - 5)) + ((y - 5) * (y - 5))) * ((((x - 1) * (x - 1)) + ((y - 3) * (y - 3))) / 8) + 400;  //TEMP +400; Original +4
             }
         }
 
@@ -34,15 +34,29 @@
             hubsDensity.Add(Tuple.Create(xRnd, yRnd, density[xRnd, yRnd]));
         }
 
+
+        var cdfDensity = new List<double>();  //Inicjacja listy zapisującej dystrybuantę dla wylosowanych wartości gęstości
+
+        double tempDist = 0;  //Zmienna tymczasowa na potrzeby obliczenia dystrybuant
         
+        for (int i = 0; i < nPeople; i++) {  //Obliczenie dystrybuant i zapisanie ich do listy distDens
+            tempDist += hubsDensity[i].Item3;
+            cdfDensity.Add(tempDist);
+        }
 
 
-
+        
 
 
         /*
         foreach (Tuple<int, int, double> tuple in hubsDensity) {
             Console.WriteLine(tuple.Item1 + " " + tuple.Item2 + " " + tuple.Item3);
+        }
+        */
+        /*
+        for (int i = 0; i < distDens.Count(); i++)
+        {
+            Console.WriteLine(distDens[i]);
         }
         */
         /*
