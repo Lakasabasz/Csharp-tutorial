49199a81d03bae0f275e85b46c24ce21f830b001
itstechsupport <maciek.z.yt.pl@gmail.com>
2023-09-10 11:18:52 +0200

    Update Program.cs

--- CloneTest/Program.cs
+++ CloneTest/Program.cs
@@ -6,13 +6,20 @@ int height = 8;
 double[,] density = new double[width, height];
 
 //deklaracja tablicy atrakcyjnosc
-double[,] attraction = new double[width, height];
-
+double[,] desirability = new double[width, height];
 
 //int x = 1;
 //int y = 1;
 int a = 1;
 int b = 1;
+
+//deklaracja listy wylosowanych elementow z tablicy gestosci
+var hubsDensity = new List<Tuple<int, int, double>>();
+
+//deklaracja listy wylosowanych elementow z tablicy atrakcyjnosci
+var hubsDesirability = new List<Tuple<int, int, double>>();
+
+Random rnd = new Random();
 
 //iterujemy sobie przez wszystkie elementy tablicy
 for (int x = 0; x < width; x++)
@@ -25,14 +32,27 @@ for (int x = 0; x < width; x++)
         //Console.WriteLine(value);
         //i tablicy atrakcyjnosci
         double value_ = -Math.Abs((x - a / 2.0) * (y - b / 2.0)) + (a / 2.0 * b / 2.0);
-        attraction[x, y] = value_;
+        desirability[x, y] = value_;
         //Console.WriteLine(value_); 
     }
 }
 
-//wybieranie losowych komorek
-Random rnd = new Random();
-int rnd_w = rnd.Next(0, width);
-int rnd_h = rnd.Next(0, height);
-Console.WriteLine($"wartosc elementu z tablicy gestosci na {rnd_w}:{rnd_h} = {density[rnd_w, rnd_h]}");
-Console.WriteLine($"wartosc elementu z tablicy gestosci na {rnd_w}:{rnd_h} = {attraction[rnd_w, rnd_h]}");
+//wybieranie losowych elementow
+void draw_elements(int i)
+{
+
+    while (i > 0){
+        //losowanie do tablicy gestosci
+        int rnd_w = rnd.Next(0, width);
+        int rnd_h = rnd.Next(0, height);
+        hubsDensity.Add(Tuple.Create(rnd_w, rnd_h, density[rnd_w, rnd_h]));
+        //losowanie do tablicy atrakcyjnosci
+        rnd_w = rnd.Next(0, width);
+        rnd_h = rnd.Next(0, height);
+        hubsDesirability.Add(Tuple.Create(rnd_w, rnd_h, desirability[rnd_w, rnd_h]));
+
+        i--;
+    }
+}
+
+draw_elements(5);
